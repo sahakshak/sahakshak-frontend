b29b632852c09cfb7483bdedd8975463ddd211e4
Atif Ali <atif276ali@gmail.com>
2024-02-03 16:01:15 +0530

    Removed Maps

--- app/dashboard/map/page.tsx
+++ app/dashboard/map/page.tsx
@@ -5,12 +5,5 @@ import { TileLayer } from "react-leaflet/TileLayer";
 import { useMap } from "react-leaflet/hooks";
 
 export default function page() {
-  return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
-  );
+  return <div>Test</div>;
 }
